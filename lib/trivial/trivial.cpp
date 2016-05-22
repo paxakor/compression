28a647e62cd7ee3828f44ea59f6a71c92150f643
Pavel Korozevtsev <paxakor@yandex.ru>
2016-05-22 14:12:43 +0300

    trivial codec added (for testing)

---
+++ lib/trivial/trivial.cpp
@@ -0,0 +1,28 @@
+// Copyright 2016, Pavel Korozevtsev.
+
+#include <string>
+#include "trivial/trivial.h"
+
+namespace Codecs {
+
+void TrivialCodec::encode(string& encoded, const string_view& raw) const {
+  encoded = raw.to_string();
+}
+
+void TrivialCodec::decode(string& raw, const string_view& encoded) const {
+  raw = encoded.to_string();
+}
+
+string TrivialCodec::save() const { }
+
+void TrivialCodec::load(const string_view& config) { }
+
+void TrivialCodec::learn(const vector<string_view>& all_samples) { }
+
+size_t TrivialCodec::sample_size(size_t records_total) const {
+  return 0;
+}
+
+void TrivialCodec::reset() { }
+
+}  // namespace Codecs
---
+++ lib/trivial/trivial.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "codec/codec.h"
+
+namespace Codecs {
+
+class TrivialCodec : public CodecIFace {
+public:
+  void encode(string&, const string_view&) const override;
+  void decode(string&, const string_view&) const override;
+
+  string save() const override;
+  void load(const string_view&) override;
+
+  size_t sample_size(size_t) const override;
+  void learn(const vector<string_view>&) override;
+
+  void reset() override;
+};
+
+}  // namespace Codecs
