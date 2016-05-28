7b8dda5cd469ba48a38dcaa0954dbd676b0954be
Pavel Korozevtsev <paxakor@yandex.ru>
2016-05-28 14:55:03 +0300

    nothing

--- testers/stopwatch.h
+++ testers/stopwatch.h
@@ -13,16 +13,19 @@ public:
   , start(clock()) {
     std::cout << "Start " << name << std::endl;
   }
+
   void end() {
     double secs = (clock() - start) / CLOCKS_PER_SEC;
     std::cout << name << " ended in " << secs << " seconds" << std::endl;
     this->ended = true;
   }
+
   ~Stopwatch() {
     if (!ended) {
       this->end();
     }
   }
+
 private:
   const std::string name;
   double start;
